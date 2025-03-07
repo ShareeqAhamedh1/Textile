81a4219da5bfdb8ca3a83d9df7c5f998e8788688
mnmnaveed <44447969+mnmnaveedshadow@users.noreply.github.com>
2025-03-07 15:04:59 +0530

    opening balance

--- backend/addExpence.php
+++ backend/addExpence.php
@@ -10,14 +10,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $currDate=date('Y-m-d');
     // Fetch yesterday's closing balance as today's opening balance
-    if($category == 'Opening Balance' && $expense_date != $currDate){
-      $sql_opening_balance = "SELECT * FROM tbl_expenses WHERE expense_date = '$currDate' AND category='Opening Balance'";
+
+      $sql_opening_balance = "SELECT * FROM tbl_expenses WHERE expense_date = '$expense_date' AND category='Opening Balance'";
       $rs_opening_balance = $conn->query($sql_opening_balance);
       if ($rs_opening_balance->num_rows > 0) {
           header("Location: ../manage_expenses.php?status=fail");
           exit();
       }
-    }
+  
+
 
 
 
