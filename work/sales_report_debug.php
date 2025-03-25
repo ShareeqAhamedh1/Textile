1f340db72da61367325312eb76ea24d605be7716
mnmnaveed <44447969+mnmnaveedshadow@users.noreply.github.com>
2025-03-25 14:11:03 +0530

    error fixed

--- work/sales_report_debug.php
+++ work/sales_report_debug.php
@@ -7,3 +7,34 @@
   <td> <?= getProductCost($conn,$p_id) ?> </td>
   <td>LKR <?= number_format(getProductPrice($conn,$p_id) - getProductCost($conn,$p_id),2) ?> </td>
 <?php } ?>
+
+
+<?php
+  if($customer_id !=0){
+ ?>
+ var cccc_id = <?= $customer_id ?>;
+
+ if (cccc_id) {
+   $.ajax({
+     url: "backend/set_customer_session.php",
+     type: "POST",
+     data: { customer_id: cccc_id },
+     dataType: "json",
+     success: function (data) {
+       if (data.status === "success") {
+         $("#customerPhone").text(data.phone);
+         $("#customerBalance").text(parseFloat(data.balance).toFixed(2));
+         $("#customerInfoBox").fadeIn(); // Show the info box
+       } else {
+         alert("Failed to fetch customer details.");
+       }
+     },
+     error: function (xhr, status, error) {
+       console.error("AJAX Error:", error);
+       console.error("Status:", status);
+       console.error("Response Text:", xhr.responseText);
+       alert("An error occurred while fetching customer details. Check the console for details.");
+     }
+   });
+ }
+ <?php } ?>
